686d4f8b337cad07c52609ce739dc08debb94428
iGus12 <ag.varas@duocuc.cl>
2026-04-28 19:47:43 -0400

    actualizacion del authcontroller

--- src/main/java/ms_login/ms_login/controller/AuthController.java
+++ src/main/java/ms_login/ms_login/controller/AuthController.java
@@ -1,9 +1,13 @@
 package ms_login.ms_login.controller;
-//gus gay
+
 import ms_login.ms_login.dto.LoginRequest; 
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+// Importaciones necesarias para devolver el JSON
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -13,7 +17,17 @@ public class AuthController {
     public ResponseEntity<?> procesarLogin(@RequestBody LoginRequest peticionReact) {
         
         String correoRecibido = peticionReact.getEmail();
-        return ResponseEntity.ok("Conexión exitosa. Recibimos el correo: " + correoRecibido);
+        
+       
+        String jwtSimulado = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJndXN0YXZvIn0.FirmaSimuladaSanosYSalvos123";
+        
+        
+        Map<String, String> respuesta = new HashMap<>();
+        respuesta.put("email", correoRecibido);
+        respuesta.put("token", jwtSimulado);
+        respuesta.put("status", "200 OK");
+        
+        return ResponseEntity.ok(respuesta);
         
     }
 }
