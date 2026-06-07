f5e997955e7e21e82e26ca3f56e7e06030a971dd
lucastupai <luc.puentes@duocuc.cl>
2026-06-07 19:49:29 -0400

    JWT
    jwt nuevo con token

--- src/main/java/ms_login/ms_login/service/LoginServiceImpl.java
+++ src/main/java/ms_login/ms_login/service/LoginServiceImpl.java
@@ -3,8 +3,8 @@ package ms_login.ms_login.service;
 import ms_login.ms_login.dto.LoginRequest;
 import ms_login.ms_login.dto.LoginResponse;
 import ms_login.ms_login.model.Usuario;
-import ms_login.ms_login.repository.UsuarioRepository; 
-import ms_login.ms_login.util.JwtService; 
+import ms_login.ms_login.repository.UsuarioRepository;
+import ms_login.ms_login.util.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +17,7 @@ public class LoginServiceImpl implements ILoginService {
     private UsuarioRepository repository;
 
     @Autowired
-    private JwtService jwtService; 
+    private JwtService jwtService;
 
     @Override
     public Usuario registrar(Usuario usuario) {
@@ -26,21 +26,16 @@ public class LoginServiceImpl implements ILoginService {
 
     @Override
     public LoginResponse login(LoginRequest request) {
-        
+
         Optional<Usuario> userOpt = repository.findByUsername(request.getUsername());
-        
-       
+
         if (userOpt.isPresent() && userOpt.get().getPassword().equals(request.getPassword())) {
-            
-           
+
             String token = jwtService.generarToken(userOpt.get().getUsername());
-            
-           
-            return new LoginResponse(token, "Login Exitoso", true);
+
+            return new LoginResponse(token, "Login exitoso", true);
         }
-        
-        
+
         return new LoginResponse("Credenciales incorrectas", false);
     }
 }
-    
